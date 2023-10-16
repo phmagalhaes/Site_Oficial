2772111c5f2a6c8684e7937a94ca58970028666f
Pedro Magalhães <142751028+phmagalhaes@users.noreply.github.com>
2023-10-16 20:22:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+document.getElementById('aprenderHTML').addEventListener('click', function () {
+    window.location.href = 'leia_html.html'
+});
+
+document.getElementById('aprenderCSS').addEventListener('click', function () {
+    window.location.href = 'leia_css.html'
+});
+
+document.getElementById('index').addEventListener('click', function () {
+    window.location.href = 'index.html'
+});
+
+document.getElementById('login').addEventListener('click', function () {
+    window.location.href = 'index.html'
+});
+
+document.getElementById('cadastrar').addEventListener('click', function () {
+    window.location.href = 'cadastro.html'
+});
+
+document.getElementById('voltar').addEventListener('click', function () {
+    window.location.href = 'login.html'
+});
+
+document.getElementById('ok').addEventListener('click', function () {
+    window.location.href = 'login.html'
+});
